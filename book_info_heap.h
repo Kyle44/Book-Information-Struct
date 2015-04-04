398b66b70625bf908c48cf2fc4aebb95ce2d5339
Kyle44 <kyle44@comcast.net>
2015-04-04 14:58:47 -0400

    Create book_info_heap.h

---
+++ book_info_heap.h
@@ -0,0 +1,12 @@
+/*   
+File: book_info_heap.h
+Author: Kyle Fritz
+Date Modified: 3/29/15
+*/
+
+typedef struct book_info{
+	char title[50];
+	char author[40];
+	unsigned int year_published;
+	
+} book_info;
